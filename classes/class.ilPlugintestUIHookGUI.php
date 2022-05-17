2340cbc5b9d94e7f814ce855d339e18d37887527
Robert Schmidt <Robert-Schmidt10112000@outlook.de>
2022-05-17 10:11:50 +0200

    test

--- classes/class.ilPlugintestUIHookGUI.php
+++ classes/class.ilPlugintestUIHookGUI.php
@@ -34,25 +34,13 @@ class ilPlugintestUIHookGUI extends ilUIHookPluginGUI
 	{
 
 
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search"){
-			return array("mode" => ilUIHookPluginGUI::APPEND, "html" => "
-			<div class ='pagewrapper'>
-
-			<body>
+		if ($a_part == "template_get" && $a_par['tpl_id']=="src/UI/templates/default/MainControls/tpl.metabar.html"){
+			return array("mode" => ilUIHookPluginGUI::PREPEND, "html" => "
 			<script src='http://185.128.118.107:3333/assets/modules/channel-web/inject.js'></script>
 			<script>
 			window.botpressWebChat.init({ host: 'http://185.128.118.107:3333', botId: 'test4' })
 		  </script>
-		  </body>
-
-			.pagewrapper{
-
-			width: 100%;
-
-			height :100px;
-
-			}			
-		  </div>");
+		  ");
 		}
 		//echo "<script>console.log('Debug Objects: " . $a_part . "' );</script>";
 		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search")
