52f34f53a48117d1331a4b68afce1d7e06d00ee3
Robert Schmidt <Robert-Schmidt10112000@outlook.de>
2022-05-17 12:51:04 +0200

    test

--- classes/class.ilPlugintestUIHookGUI.php
+++ classes/class.ilPlugintestUIHookGUI.php
@@ -34,7 +34,7 @@ class ilPlugintestUIHookGUI extends ilUIHookPluginGUI
 	{
 
 
-		if ($a_comp == "Services/MainMenu"){
+		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search"){
 			return array("mode" => ilUIHookPluginGUI::PREPEND, "html" => "
 			<script src='http://185.128.118.107:3333/assets/modules/channel-web/inject.js'></script>
 			<script>
@@ -43,70 +43,7 @@ class ilPlugintestUIHookGUI extends ilUIHookPluginGUI
 		  ");
 		}
 		//echo "<script>console.log('Debug Objects: " . $a_part . "' );</script>";
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search")
-		{
-			// $a_par["main_menu_gui"] is ilMainMenu object
-			// $a_par["main_menu_search_gui"] is ilMainMenuSearchGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND, "html" => "");
-		}
-
-
-		// do not show the search part of the main menu
-		// $a_par["main_menu_gui"]
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search")
-		{
-			// $a_par["main_menu_gui"] is ilMainMenu object
-			// $a_par["main_menu_search_gui"] is ilMainMenuSearchGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::REPLACE, "html" => "");
-		}
 		
-		// add something to the main menu entries
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_list_entries")
-		{
-			// $a_par["main_menu_gui"] is ilMainMenu object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND, "html" =>
-				" <a class='MMInactive' href='http://www.ilias.de' target='_blank'>ILIAS Home Page</a>");
-		}
-		
-		// add something to locator
-		if ($a_comp == "Services/Locator" && $a_part == "main_locator")
-		{
-			// $a_par["locator_gui"] is ilLocatorGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND,
-				"html" => " » My Locator");
-		}
-
-		// add things to the personal desktop overview
-		if ($a_comp == "Services/PersonalDesktop" && $a_part == "left_column")
-		{
-			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::PREPEND,
-				"html" => "Prepend to left");
-		}
-
-		// add things to the personal desktop overview
-		if ($a_comp == "Services/PersonalDesktop" && $a_part == "right_column")
-		{
-			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND,
-				"html" => "Append to right");
-		}
-
-		// add things to the personal desktop overview
-		if ($a_comp == "Services/PersonalDesktop" && $a_part == "center_column")
-		{
-			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::PREPEND,
-				"html" => "Prepend to center");
-		}
-
 		return array("mode" => ilUIHookPluginGUI::KEEP, "html" => "");
 	}
 
@@ -120,12 +57,5 @@ class ilPlugintestUIHookGUI extends ilUIHookPluginGUI
 		// here. So you need to use general GET/POST information
 		// like $_GET["baseClass"], $ilCtrl->getCmdClass/getCmd
 		// to determine the context.
-		if ($a_part == "tabs")
-		{
-			// $a_par["tabs"] is ilTabsGUI object
-			
-			// add a tab (always)
-			$a_par["tabs"]->addTab("test", "test", "test");
-		}
 	}
 }
